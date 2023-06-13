8667e26da13d091bd17e9ab276e1122d84ed531d
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2023-06-13 10:01:19 +1000

    Create util.py

---
+++ util.py
@@ -0,0 +1,27 @@
+prompt_prefix = """Please translate the following words from Hmong to English: \
+miv - cat, \
+lig - late, \
+pov - throw, \
+khob - cup, \
+dav - wide, \
+nce - ascend, \
+tsheb - car, \
+ntxov - early, \
+hiab - weave, \
+toj - hill, \
+da dej - bathe, \
+faib - share, \
+xeeb ntxwv - grandchildren, \
+pib - start, \
+hniav - tooth, \
+ntxuav - wash, \
+ntiaj teb - world, \
+soj ntsuam - investigate, \
+kua mis - milk, \
+txias - cool, \
+xiav - blue, \
+cob qhia - instruct, \
+vwj - crazy, \
+dais - bear, \
+lwj - spoiled, \
+nyeem - read,"""
