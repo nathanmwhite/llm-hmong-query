ff06e8792c905f2a9b4e254ca2402f111f4a61ff
nathanmwhite <53862826+nathanmwhite@users.noreply.github.com>
2023-06-15 11:01:08 +1000

    Update bloom_query.py

--- bloom_query.py
+++ bloom_query.py
@@ -1,14 +1,24 @@
 import json
 import requests
+
+from .util import prompt_prefix, words
+
 API_URL = "https://api-inference.huggingface.co/models/bigscience/bloom"
 API_TOKEN = "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"
-headers = {"Authorization": f"Bearer {API_TOKEN}"}
-def query(payload):
+headers = {"Authorization": f"Bearer {API_TOKEN}",
+           "Content-Type": "application/json"}
+def query(text):
+    payload = {"inputs": text,
+           "parameters": {"max_new_tokens": 32,
+                          "return_full_text": False},
+           "options": {"use_cache": False, "wait_for_model": True}}
     data = json.dumps(payload)
     response = requests.request("POST", API_URL, headers=headers, data=data)
     return json.loads(response.content.decode("utf-8"))
   
-# TODO: define
-prompt = ""
-
-data = query(prompt)
+def loop_words():
+    results = []
+    for w in words.keys():
+        data = query(prompt_prefix + w + " - ")
+        results.append((w, data))
+    return results
